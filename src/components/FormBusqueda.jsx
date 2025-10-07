58fedb60a7f55c97a96df73dac34220fb1f142c0
dzuccolilli <dzuccolilli@gmail.com>
2025-10-07 17:02:51 +0000

    segundo commit

---
+++ src/components/FormBusqueda.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const FormBusqueda = () => {
+    const[Busqueda, setBusqueda] = useState("");
+    const submit =(e)=>{
+        e.preventDefault();
+        console.log(Busqueda);
+    }
+    return (
+        <form onSubmit={submit} value={Busqueda} onChange={(e)=>setBusqueda(e.target.value)}  action="" method='get'>
+            <input type="text" placeholder="Buscar"></input>
+            <button type="submit">Buscar</button>
+        </form>
+    );
+}
+
+export default FormBusqueda;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import FormBusqueda from "./FormBusqueda.jsx";
+import HeaderStyle from "../styles/Header.module.css";
+import carrito from "../assets/icons/carrito-de-compras.png";
 
-function Header(){
-    return(
-        <header style={{fontFamily: 'sans-serif',
-     margin: '20px', textAlign:"center" }}>
-            <h1>Comenzando el proyecto</h1>
-        </header>
+function Header() {
+  
+    return (
+        <header style={{
+            fontFamily: 'sans-serif',
+            margin: '20px', textAlign: "center"
+        }}>
+            <div className={HeaderStyle.headerH4}><h4>Realiza tus pedidos y coordina la entrega, con 48hs de anticipacion</h4></div>
+            <div className={HeaderStyle.header}>
+            <FormBusqueda/>
+            <h1 className={HeaderStyle.title}>Mis Princesas Candybar</h1>
+            <div className={HeaderStyle.links}>
+                <div><a href="">Crear cuenta</a>
+                    <a href="">Iniciar sesión</a>
+                </div>
+                <div className={HeaderStyle.cart}>
+                    
+                   
+                        <a href=""><img className={HeaderStyle.imgCart} src={carrito} alt="" />
+                        <span>0</span>
+                        </a>
+                        
+                   
+              
+                </div>
+            </div>
+        </div>
+        </header >
     );
 }
-    
+
 export default Header;
