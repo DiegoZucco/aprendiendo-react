4a16299758bf9b2c9aae004de506272d3fcccf91
dzuccolilli <dzuccolilli@gmail.com>
2025-11-19 10:56:50 +0000

    rutas protegidas, navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,52 @@
 import React from 'react';
 import Header from "./components/Header.jsx";
 import Nav from "./components/Nav.jsx";
-import Main from "./components/Main.jsx";
 import style from "./styles/index.module.css";
+import Carrito from './components/Carrito.jsx';
+import { useState } from 'react';
+import Inicio from './pages/Inicio.jsx';
+import ProductosPag from './pages/ProductosPag.jsx';
+import {Routes, Route } from 'react-router-dom';
+import Contacto from './pages/Contacto.jsx';
+import DetalleArticulo from './components/DetalleArticulo.jsx';
 
 
 function App() {
-  const text = ["Inicio", "Nuestros Productos","Sobre Nosotros", "Contacto"];
-  
+  const navItems = [{text:"Inicio", path:"/"},
+    {text: "Nuestros Productos", path:"/productos"},
+    {text: "Sobre Nosotros", path:"/sobre-nosotros"},
+    {text: "Contacto", path:"/contacto"}];
+
+  const [carrito, setCarrito] = useState([]);
+
+  const agregarAlCarrito = (data) => {
+    // Mejorado para no añadir duplicados
+    if (!carrito.find(item => item.id === data.id)) {
+        setCarrito([...carrito, data]);
+    }
+  };
+
+  // Restauramos la función original de vaciar carrito
+  const vaciarCarrito = () => {
+    setCarrito(carrito.slice(0, carrito.length - 1));
+  };
+
   return (
-      <div className={style.App}>
-      <Header/>
-      <Nav items= {text}/>
-      <Main/>
-     </div>
-     
+    <div className={style.App}>
+      <Header carrito={carrito} />
+      <Nav items={navItems} />
+      <Routes>
+        <Route path ="/productos/:id" element={<DetalleArticulo agregarAlCarrito={agregarAlCarrito}/>}/>
+        <Route path="/" element={<Inicio agregarAlCarrito={agregarAlCarrito} />}/>
+        <Route path ="/contacto" element={<Contacto/>}/>
+        <Route path = "/productos" element ={<ProductosPag carrito={carrito} agregarAlCarrito={agregarAlCarrito} />}/>
+        
+        {/* --- CAMBIO REVERTIDO --- */}
+        {/* Volvemos a pasar la prop `vaciarCarrito` original */}
+        <Route path = "/carrito" element ={<Carrito carrito={carrito} vaciarCarrito={vaciarCarrito}/>}/>
+      </Routes>
+    </div>
+
   );
 }
 
